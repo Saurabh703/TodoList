59fe3b2dff6c06f915876e2b7f25ae457e98f53d
Saurabh Pilare <78712987+Saurabh703@users.noreply.github.com>
2022-09-05 22:45:16 +0530

    Add my file

---
+++ src/ToDoLists.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ToDoLists = (props) => {
+
+    
+    return(
+     <>
+     <div className="todo_style">
+      {/* <i className="fa fa-times" 
+         aria-hidden="true"  */}
+        <div className="fa fa-times"
+        onClick={() => {
+             props.onSelect(props.id)
+         }}
+     />
+     <li>{props.text}</li>
+     </div>
+     </>
+    );
+};
+
+export default ToDoLists;
